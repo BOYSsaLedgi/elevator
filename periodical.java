216fd888d87a536c16c8d50e5c589bf3ee20a912
BOYSsaLedgi <boyssaledgi@gmail.com>
2023-04-18 22:33:34 +0800

    eyyy functions complete

--- periodical.java
+++ periodical.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.lang.*;
+import java.util.Date; 
 
 public class periodical {
     //input
@@ -10,32 +11,78 @@ public class periodical {
         //java delay elevator moving, sysout, elevator opening, 
     //if absolute difference is the same but different floor, when Up is pressed, the one on the bottom will go to you.
     //if down is pressed, the one on the top will go to you.
+
+    //MAIN FUNCTION//
     public static void main(String[] args) {
         Scanner scan = new Scanner (System.in);
 
-        int ElevatorLocation;
-        int character = 3;
-        int elevator1 = 5;
-        int elevator2 = 1;
+        int ElevatorLocation; //to get which elevator is nearest to you
+        int character = 1; //variable for character location
+        int elevator1 = 5; //variable for  elevator1 location
+        int elevator2 = 7; //variable for elevator 2 location
+
         ElevatorLocation = ElevatorCheck(character, elevator1, elevator2);
         ElevatorStatus(ElevatorLocation, character, elevator1, elevator2);
-        //pass the character level to the elevator going up
-        //elvator delay
-        //opening door , tell what the floor the elevator is in now.
-        //delayy 1 second
-        //door opened
 
+        //elevator delay
+        int AbsFloorDiff = ReturnAbsDifference(character, elevator1, elevator2);
+        Delay(2000 * AbsFloorDiff); //It takes 2 seconds to travel in each floor.
+
+        //pass the character level to the elevator going up
+        if (ElevatorLocation==1)
+        {
+            elevator2 = character;
+            System.out.println("elevator 2: " + elevator2);
+        }
+        else if (ElevatorLocation==2)
+        {
+            elevator1 = character;
+            System.out.println("elevator 1: " + elevator1);
+        }
+        else if(ElevatorLocation==3)
+        {
+            elevator2 = character;
+            System.out.println("elevator 2: " + elevator2);
+        }
+        
+        //opening door , tell what the floor the elevator is in now.
+        System.out.print("Elevator 1: " + elevator1);
+        System.out.println(" || Elevator 2: " + elevator2);
+
+        //delayy 1 second
+        Delay(1000); //it takes 1 second for the door to open
+
+        //door opened
+        System.out.println("Open sesame");
+
+        //delay 1 second
+        Delay(1000); //it takes 1 second for the door to close
+
+        //door closed
+        System.out.println("Door closed");
     }
 
-    public static int ElevatorCheck(int charlvl, int elev1, int elev2)
+    public static void Delay(int x) //waiting for few seconds
     {
-        int difference1 = charlvl-elev1;
-        int difference2 = charlvl-elev2;
+        //System.out.println("Delaying");
+        try
+        {
+            Thread.sleep(x);
+        }
+        catch (InterruptedException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+    public static int ElevatorCheck(int charlvl, int elev1, int elev2) //to check which elevator is nearest to you
+    {
+        int difference1 = charlvl-elev1; //how far elev1 from character
+        int difference2 = charlvl-elev2; //how far elev2 from character
 
-        int absdiff1 = Math.abs(difference1);
-        int absdiff2 = Math.abs(difference2);
+        int absdiff1 = Math.abs(difference1); //absolute value of elev1
+        int absdiff2 = Math.abs(difference2); //absolute value of elev2
 
-        int x=0;
+        int x=0; //x will serve as return value
 
         if (absdiff1>absdiff2)
         {
@@ -47,7 +94,31 @@ public class periodical {
         }
         else if (absdiff1==absdiff2)
         {
-            x = 3; //both on the same floor
+            x = 3; //both on the same floor or same distance
+        }
+        return x;
+    }
+    public static int ReturnAbsDifference (int charlvl, int elev1, int elev2) //basically the same with ElevatorCheck() but only returns the abs floor difference
+    {
+        int difference1 = charlvl-elev1; //how far elev1 from character
+        int difference2 = charlvl-elev2; //how far elev2 from character
+
+        int absdiff1 = Math.abs(difference1); //absolute value of elev1
+        int absdiff2 = Math.abs(difference2); //absolute value of elev2
+
+        int x=0; //x will serve as return value
+
+        if (absdiff1>absdiff2)
+        {
+            x = absdiff2; //elevator 2 is nearer
+        }
+        else if (absdiff1<absdiff2)
+        {
+            x = absdiff1; //elevator 1 is nearer
+        }
+        else if (absdiff1==absdiff2)
+        {
+            x = absdiff2; //both on the same floor or same distance
         }
         return x;
     }
