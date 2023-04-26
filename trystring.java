44a843e1040f46cbcc3040165a81dac9a1efd754
BOYSsaLedgi <boyssaledgi@gmail.com>
2023-04-26 14:38:25 +0800

    Update trystring.java

--- trystring.java
+++ trystring.java
@@ -1,12 +1,30 @@
 public class trystring {
     public static void main(String[] args) {
+        int ElevatorCheck = 3;
+        if (ElevatorCheck==1 || ElevatorCheck==3)
+        {
+            topdesign(3, 4);
+            RightElevOpening();
+            Delay(3000);
+            DeleteLine(5);
+            RightElevClosing();
+        }
+        else if (ElevatorCheck==2)
+        {
+            topdesign(ElevatorCheck, ElevatorCheck);
+            LeftElevOpening();
+            Delay(3000);
+            DeleteLine(5);
+            LeftElevClosing();
+        }
+        /* 
         topdesign(6, 7);
-        ElevOpening();
+        LeftElevFullOpen();
+        Delay(2000);
         DeleteLine(5);
         ElevClosing();
         topdesign(6, 7);
-        ElevIdle();
-        
+        ElevIdle();*/
         System.out.print("COME IN YO!");
     }
     public static void topdesign(int x, int y)
@@ -23,6 +41,93 @@ public class trystring {
         System.out.println("|  |     |      |   "+5+"  |     |      | |");
         System.out.println("|__|_____|______|______|_____|______|_|");
     }
+
+    public static void RightElevHalfOpen()
+    {
+        System.out.println("|  ______________   "+5+"  ______________ |");
+        System.out.println("|  |     |      |   "+5+"  |    | |     | |");
+        System.out.println("|  |     |      |   "+5+"  |    | |     | |");
+        System.out.println("|  |     |      |   "+5+"  |    | |     | |");
+        System.out.println("|__|_____|______|______|____|_|_____|_|");
+    }
+
+    public static void RightElevFullOpen()
+    {
+        System.out.println("|  ______________   "+5+"  ______________ |");
+        System.out.println("|  |     |      |   "+5+"  | |        | | |");
+        System.out.println("|  |     |      |   "+5+"  | |        | | |");
+        System.out.println("|  |     |      |   "+5+"  | |        | | |");
+        System.out.println("|__|_____|______|______|_|________|_|_|");
+    }
+
+    public static void RightElevOpening()
+    {
+        ElevIdle();
+        Delay(1000);
+        DeleteLine(5);
+
+        RightElevHalfOpen();
+        Delay(1000);
+        DeleteLine(5);
+
+        RightElevFullOpen();
+    }
+    public static void RightElevClosing()
+    {
+        RightElevFullOpen();
+        Delay(1000);
+        DeleteLine(5);
+
+        RightElevHalfOpen();
+        Delay(1000);
+        DeleteLine(5);
+
+        ElevIdle();
+    }
+
+    public static void LeftElevHalfOpen()
+    {
+        System.out.println("|  ______________   "+5+"  ______________ |");
+        System.out.println("|  |    | |     |   "+5+"  |     |      | |");
+        System.out.println("|  |    | |     |   "+5+"  |     |      | |");
+        System.out.println("|  |    | |     |   "+5+"  |     |      | |");
+        System.out.println("|__|____|_|_____|______|_____|______|_|");
+    }
+
+    public static void LeftElevFullOpen()
+    {
+        System.out.println("|  ______________   "+5+"  ______________ |");
+        System.out.println("|  | |        | |   "+5+"  |     |      | |");
+        System.out.println("|  | |        | |   "+5+"  |     |      | |");
+        System.out.println("|  | |        | |   "+5+"  |     |      | |");
+        System.out.println("|__|_|________|_|______|_____|______|_|");
+    }
+
+    public static void LeftElevOpening()
+    {
+        ElevIdle();
+        Delay(1000);
+        DeleteLine(5);
+
+        LeftElevHalfOpen();
+        Delay(1000);
+        DeleteLine(5);
+
+        LeftElevFullOpen();
+    }
+    public static void LeftElevClosing()
+    {
+        LeftElevFullOpen(); 
+        Delay(1000);
+        DeleteLine(5);
+
+        LeftElevHalfOpen();
+        Delay(1000);
+        DeleteLine(5);
+
+        ElevIdle();
+    }
+
     public static void ElevOpening() {
 
         DesignElevClosed();
